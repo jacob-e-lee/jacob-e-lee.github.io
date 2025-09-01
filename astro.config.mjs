ed313dbf14e9dc1f4624860af5e5abc9cfe78294
Jacob Lee <jelee231@gmail.com>
2025-09-01 16:00:07 -0400

    Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jelee231.github.io',
+
   vite: {
       plugins: [tailwindcss()],
   },
